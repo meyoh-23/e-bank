de351cd02eba815e0bc55881678e96c3e43238c9
felix Ouma <moxiemeyoh@gmail.com>
2023-10-16 18:32:02 +0300

    completed the resposive Navbar
    a resposive navbar for both the mobile and the deskotp versions have been completed.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import Navbar from "./components/Navbar";
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+      xxl: 1400
+    }
+  },
+  palette: {
+    primary: {
+      main: "hsl(0, 0%, 100%)",
+    },
+    secondary: {
+      main: "hsl(233, 26%, 24%)"
+    },
+    neutral: {
+      main: "hsl(233, 8%, 62%)"
+    },
+  }
+})
+
 
 function App() {
   return (
+    <> 
+    <ThemeProvider theme={theme}>
+    <CssBaseline/>
     <div className="App">
-    e-bank project
+      <Navbar/>
     </div>
+    </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,353 @@
+import { AppBar, Box, Button, Container, Drawer, Link, List, ListItem, Toolbar} from '@mui/material'
+import React, { useState } from 'react';
+import logo from './../assets/logo.svg';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
+
+const Navbar = () => {
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const colorGrandient = 'linear-gradient(to right, hsl(136, 65%, 51%), hsl(192, 70%, 51%))'
+
+    return (
+        <AppBar
+        component='nav'
+        elevation={0}
+        >
+            <Toolbar>
+                <Container
+                sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+            }}
+                >
+                    <Box
+                    sx={{
+                        display: "inline-block",
+                        textOverflow: "no-wrap",
+                        cursor: "pointer"
+                    }}
+                    >
+                        <span><img src={logo} alt="logo" /></span>
+                    </Box>
+                    <Box
+                    sx={{
+                        display: {
+                            xs: " none",
+                            md: "flex",
+                        },
+                    }}
+                    >
+                        <List
+                        sx={{
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "space-between",
+                            alignItems: "flex-end",
+                        }}
+                        >
+                            <ListItem>
+                                <Link
+                                href='#home'
+                                underline='none'
+                                sx={{
+                                    color: "hsl(233, 8%, 62%)",
+                                    fontFamily: "'Public Sans', sans-serif",
+                                    fontWeight: "300",
+                                    fontSize: "1.125rem",
+                                    textTransform: "capitalize",
+                                    py: "1.25rem",
+                                    "&:hover": {
+                                        color: "hsl(233, 26%, 24%)",
+                                        borderBottom: "4px solid hsl(136, 65%, 51%)",
+                                        cursor: "pointer"
+                                    }
+                                }}
+                                >
+                                home
+                                </Link>
+                            </ListItem>
+                            <ListItem>
+                                <Link
+                                href='#about'
+                                underline='none'
+                                sx={{
+                                    color: "hsl(233, 8%, 62%)",
+                                    fontFamily: "'Public Sans', sans-serif",
+                                    fontWeight: "300",
+                                    fontSize: "1.125rem",
+                                    textTransform: "capitalize",
+                                    py: "1.25rem",
+                                    "&:hover": {
+                                        color: "hsl(233, 26%, 24%)",
+                                        borderBottom: "4px solid hsl(136, 65%, 51%)",
+                                        cursor: "pointer"
+                                    }
+                                }}
+                                >
+                                about
+                                </Link>
+                            </ListItem>
+                            <ListItem>
+                                <Link
+                                href='#contact'
+                                underline='none'
+                                sx={{
+                                    color: "hsl(233, 8%, 62%)",
+                                    fontFamily: "'Public Sans', sans-serif",
+                                    fontWeight: "300",
+                                    fontSize: "1.125rem",
+                                    textTransform: "capitalize",
+                                    py: "1.25rem",
+                                    "&:hover": {
+                                        color: "hsl(233, 26%, 24%)",
+                                        borderBottom: "4px solid hsl(136, 65%, 51%)",
+                                        cursor: "pointer"
+                                    }
+                                }}
+                                >
+                                contact
+                                </Link>
+                            </ListItem>
+                            <ListItem>
+                                <Link
+                                href='#blog'
+                                underline='none'
+                                sx={{
+                                    color: "hsl(233, 8%, 62%)",
+                                    fontFamily: "'Public Sans', sans-serif",
+                                    fontWeight: "300",
+                                    fontSize: "1.125rem",
+                                    textTransform: "capitalize",
+                                    py: "1.25rem",
+                                    "&:hover": {
+                                        color: "hsl(233, 26%, 24%)",
+                                        borderBottom: "4px solid hsl(136, 65%, 51%)",
+                                        cursor: "pointer"
+                                    }
+                                }}
+                                >
+                                blog
+                                </Link>
+                            </ListItem>
+                            <ListItem>
+                                <Link
+                                href='#career'
+                                underline='none'
+                                sx={{
+                                    color: "hsl(233, 8%, 62%)",
+                                    fontFamily: "'Public Sans', sans-serif",
+                                    fontWeight: "300",
+                                    fontSize: "1.125rem",
+                                    textTransform: "capitalize",
+                                    py: "1.25rem",
+                                    "&:hover": {
+                                        color: "hsl(233, 26%, 24%)",
+                                        borderBottom: `4px solid hsl(136, 65%, 51%)`,
+                                        cursor: "pointer"
+                                    }
+                                }}
+                                >
+                                career
+                                </Link>
+                            </ListItem>
+                        </List>
+                    </Box>
+                    <Box
+                    sx={{
+                        display: {
+                            xs: " none",
+                            md: "flex",
+                        }
+                    }}
+                    >
+                        <Button
+                        variant='outlined'
+                        sx={{
+                            borderRadius: 8,
+                            background: colorGrandient,
+                            textTransform: "capitalize",
+                            fontFamily:"'Public Sans', sans-serif",
+                            fontSize: "1.125rem",
+                            fontWeight: "400",
+                            whiteSpace: "nowrap",
+                        }}
+                        >
+                            request Invite
+                        </Button>
+                    </Box>
+                    <Box
+                    sx={{
+                        display: {
+                            xs: "flex",
+                            md: "none",
+                        }
+                    }}
+                    >
+                        <Button
+                        variant='text'
+                        onClick={() => setMobileMenu(!mobileMenu)}
+                        size='lg'
+                        sx={{
+                            color: "#000",
+                        }}
+                        >
+                            {
+                                mobileMenu? <CloseIcon/>
+                                : <MenuIcon/>
+                            }
+                        </Button>
+                    </Box>
+                </Container>
+                <Drawer
+                role='menu'
+                anchor='top'
+                onClose={()=>setMobileMenu(false)}
+                open={mobileMenu}
+                PaperProps={{
+                    sx: {
+                        marginTop: "4rem",
+                        backgroundColor: "transparent",
+                        maxWidth: "90vw",
+                        mx: "auto",
+                        borderRadius: 2,
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }
+                }}
+                >
+                    <Box
+                    sx={{
+                        background: 'primary',
+                        height: "auto",
+                        mx: "auto",
+                        my: "1.25rem",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignContent: "center",
+                    }}
+                    >
+                        <List
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center", 
+                            justifyContent: "center",
+                            gap: 3,
+                        }}
+                        >
+                            <ListItem 
+                            sx={{
+                                justifyContent: "center",
+                                alignContent: "center",
+                                color: "secondary",
+                            }}
+                            >
+                                <Link
+                                href='#home'
+                                underline='none'
+                                color='secondary'
+                                sx={{
+                                    fontFamily: "'Public Sans', sans-serif",
+                                    fontSize: "1.125rem",
+                                    fontWeight: "400",
+                                    cursor: "pointer",
+                                    textTransform: "capitalize",
+                                }}
+                                >
+                                    home
+                                </Link>
+                            </ListItem>
+                            <ListItem color='#000'
+                            sx={{
+                                justifyContent: "center",
+                                alignContent: "center",
+                                color: "secondary",
+                            }}
+                            >
+                                <Link
+                                href='#about'
+                                underline='none'
+                                color='secondary'
+                                sx={{
+                                    fontFamily: "'Public Sans', sans-serif",
+                                    fontSize: "1.125rem",
+                                    fontWeight: "400",
+                                    cursor: "pointer",
+                                    textTransform: "capitalize",
+                                }}
+                                >
+                                    about
+                                </Link>
+                            </ListItem>
+                            <ListItem
+                            sx={{
+                                justifyContent: "center",
+                                alignContent: "center",
+                                color: "secondary",
+                            }}
+                            >
+                                <Link
+                                href='#contact'
+                                underline='none'
+                                color='secondary'
+                                sx={{
+                                    fontFamily: "'Public Sans', sans-serif",
+                                    fontSize: "1.125rem",
+                                    fontWeight: "400",
+                                    cursor: "pointer",
+                                    textTransform: "capitalize",
+                                }}
+                                >
+                                    contact
+                                </Link>
+                            </ListItem>
+                            <ListItem
+                            sx={{
+                                justifyContent: "center",
+                                alignContent: "center",
+                                color: "secondary",
+                            }}
+                            >
+                                <Link
+                                href='#blog'
+                                underline='none'
+                                color='secondary'
+                                sx={{
+                                    fontFamily: "'Public Sans', sans-serif",
+                                    fontSize: "1.125rem",
+                                    fontWeight: "400",
+                                    cursor: "pointer",
+                                    textTransform: "capitalize",
+                                }}
+                                >
+                                    blog
+                                </Link>
+                            </ListItem>
+                            <ListItem>
+                                <Link
+                                href='#careers'
+                                underline='none'
+                                color='secondary'
+                                sx={{
+                                    fontFamily: "'Public Sans', sans-serif",
+                                    fontSize: "1.125rem",
+                                    fontWeight: "400",
+                                    cursor: "pointer",
+                                    textTransform: "capitalize",
+                                }}
+                                >
+                                    careers
+                                </Link>
+                            </ListItem>
+                        </List>
+                    </Box>
+                </Drawer>
+            </Toolbar>
+        </AppBar>
+)}
+
+export default Navbar
